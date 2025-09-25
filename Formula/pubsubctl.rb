32fca4225b348adaf3ee6e77643a728de77243eb
tituscarl <titusranario@gmail.com>
2025-09-25 21:32:58 +0800

    revert sha

--- Formula/pubsubctl.rb
+++ Formula/pubsubctl.rb
@@ -9,7 +9,7 @@ class Pubsubctl < Formula
 
   on_macos do
     url "https://github.com/tituscarl/pubsubctl/releases/download/v0.0.3/pubsubctl_0.0.3_darwin_amd64.tar.gz"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    sha256 "dda6ddbd138d68fab6b0e417ac2748283b667a0bcc79a052f84877d149795452"
 
     def install
       bin.install "pubsubctl"
